07a7a24ff78ce8a5fc1f8f2c8085ad56a2c2010c
Xavier Callait <xaviercallait@gmail.com>
2025-02-08 00:19:13 -0500

    Created file for both the popup and options page

--- extension/src/hashpass/app/popup/page.tsx
+++ extension/src/hashpass/app/popup/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">HashPass</h1>
+      <h1 className="text-2xl font-bold">HashPassPOP</h1>
       <section className="mt-4 text-lg">Hello World!</section>
     </main>
   );
