32189db669375ed1178901d8abf8d5207c778e16
Xavier Callait <xaviercallait@gmail.com>
2025-02-08 02:22:50 -0500

    Moved page.tsx

--- extension/src/hashpass/app/options/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold">HashPassOPT</h1>
-        <section className="mt-4 text-lg">Hello World!</section>
-      </main>
-    );
-  }
-  
