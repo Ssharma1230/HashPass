79ba62a3b77fb4f10d38c9ec3c075207bfd4f205
Shivam Sharma <shivamsh1230@gmail.com>
2025-02-20 14:35:20 -0500

    Added functionilty to move pages within extension

--- extension/src/hashpass/app/popup/page.tsx
+++ extension/src/hashpass/app/popup/page.tsx
@@ -1,12 +1,14 @@
-import PasswordGenerator from '../security_components/password_generator';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">HashPassPOP</h1>
+      <h1 className="text-2xl font-bold">HashPass</h1>
       <section className="mt-4 text-lg">Hello World!</section>
       <div>
-        <PasswordGenerator/>
+      <Link href="/login/index.html">
+          <button>Go to Login &gt;</button>
+        </Link>
       </div>
     </main>
   );
