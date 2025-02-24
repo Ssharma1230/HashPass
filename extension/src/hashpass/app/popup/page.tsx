861516927f2b03a5672ce89a89380151da061944
samarthgirish2025 <girishs@purdue.edu>
2025-02-24 15:49:51 -0500

    Fixed merge issues

--- extension/src/hashpass/app/popup/page.tsx
+++ extension/src/hashpass/app/popup/page.tsx
@@ -1,16 +1,18 @@
-import Encryption_Tester from '../security_components/test/encryption_tester';
-import PasswordGenerator from '../security_components/components/password_generator';
+import Link from "next/link";
+import EncDecTester from '../security_components/test/encryption_tester';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">HashPassPOP</h1>
+      <h1 className="text-2xl font-bold">HashPass</h1>
       <section className="mt-4 text-lg">Hello World!</section>
       <div>
-        <PasswordGenerator/>
+      <Link href="/login/index.html">
+          <button>Go to Login &gt;</button>
+        </Link>
       </div>
       <div>
-        <Encryption_Tester/>
+        <EncDecTester/>
       </div>
     </main>
   );
