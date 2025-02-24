ee40912663d212470e7268a4364411b5c5675356
Ssharma1230 <56890279+Ssharma1230@users.noreply.github.com>
2025-02-24 15:29:32 -0500

    Update page.tsx to pass linter

--- extension/src/hashpass/app/popup/page.tsx
+++ extension/src/hashpass/app/popup/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import EncDecTool from '../security_components/enc_dec_tool';
-import PasswordGenerator from '../security_components/password_generator';
 
 export default function Home() {
   return (
