35ed8b3537e2bccc3889459aaf6b3e70f7260377
samarthgirish2025 <girishs@purdue.edu>
2025-03-09 20:43:09 -0400

    added lint ignore to lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
+ 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
@@ -11,4 +11,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"],
+  },
 ];
