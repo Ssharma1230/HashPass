ad95bb316aeea71f273b2a755f83e8420f91f9d6
Xavier Callait <xaviercallait@gmail.com>
2025-04-02 23:31:56 -0400

    Updated policy

--- aws/src/terraform/iam-policy.tf
+++ aws/src/terraform/iam-policy.tf
@@ -5,3 +5,11 @@ resource "aws_iam_role_policy" "iam-policy" {
 
   depends_on = [aws_iam_role.iam_role]
 }
+
+resource "aws_iam_role_policy" "iam-policy2" {
+  name   = "LambdaVPCAccess"
+  role   = aws_iam_role.iam_role.id
+  policy = file("${path.module}/iam-policy2.json")
+
+  depends_on = [aws_iam_role.iam_role]
+}
