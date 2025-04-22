24991dd8bb5950eebcb46c4704a668b65328b5d2
Xavier Callait <xaviercallait@gmail.com>
2025-04-22 11:08:31 -0400

    Added terraform files for lambdas

--- aws/src/terraform/api_gateway.tf
+++ aws/src/terraform/api_gateway.tf
@@ -34,6 +34,20 @@ resource "aws_apigatewayv2_integration" "insertSecurityQues_integration" {
   integration_method = "POST"
 }
 
+resource "aws_apigatewayv2_integration" "insertDomainName_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.insertDomainName_lambda.invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_integration" "getUserInfo_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.getUserInfo_lambda.invoke_arn
+  integration_method = "POST"
+}
+
 resource "aws_apigatewayv2_route" "signup" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "POST /user/signup"
@@ -50,6 +64,18 @@ resource "aws_apigatewayv2_route" "insertQuestions" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "POST /user/insertQuestions"
   target    = "integrations/${aws_apigatewayv2_integration.insertSecurityQues_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "insertDomainName" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /user/insertDomainName"
+  target    = "integrations/${aws_apigatewayv2_integration.insertDomainName_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "getUserInfo" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /user/getUserInfo"
+  target    = "integrations/${aws_apigatewayv2_integration.getUserInfo_integration.id}"
 }
 
 resource "aws_lambda_permission" "apigw-lambda" {
@@ -76,41 +102,6 @@ resource "aws_lambda_permission" "apigw-lambda_insertQues" {
   source_arn    = "arn:aws:execute-api:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:${aws_apigatewayv2_api.http_api.id}/*"
 }
 
-resource "aws_apigatewayv2_route" "user_logout" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /user/logout"
-}
-
-resource "aws_apigatewayv2_route" "user_update" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /user/update"
-}
-
-resource "aws_apigatewayv2_route" "user_delete" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /user/delete"
-}
-
-resource "aws_apigatewayv2_route" "hash_addpass" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /hash/addpass"
-}
-
-resource "aws_apigatewayv2_route" "hash_getpass" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /hash/getpass"
-}
-
-resource "aws_apigatewayv2_route" "hash_updatepass" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /hash/updatepass"
-}
-
-resource "aws_apigatewayv2_route" "hash_deletepass" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /hash/deletepass"
-}
-
 resource "aws_apigatewayv2_route" "calculateHash" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "POST /hash/calcHash"
@@ -124,11 +115,26 @@ resource "aws_apigatewayv2_integration" "calchash_integration" {
   integration_method = "POST"
 }
 
-
 resource "aws_lambda_permission" "apigw-lambda_hash" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.calcHash_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:${aws_apigatewayv2_api.http_api.id}/*"
+}
+
+resource "aws_lambda_permission" "apigw-lambda_insertDomainName" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.insertDomainName_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:${aws_apigatewayv2_api.http_api.id}/*"
+}
+
+resource "aws_lambda_permission" "apigw-lambda_getUserInfo" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.getUserInfo_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:${aws_apigatewayv2_api.http_api.id}/*"
 }
