0fb170c1329cc77f0cf8cefc16f8f45549a7ff71
Shivam Sharma <shivamsh1230@gmail.com>
2025-04-21 23:26:57 -0400

    Domain name added to table upon signup

--- extension/src/hashpass/app/site_signup_popup/site_signup_component.tsx
+++ extension/src/hashpass/app/site_signup_popup/site_signup_component.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useState } from "react";
 import { decrypt } from "../security_components/tools/AES_tool"
 import {calculatePassword} from '../security_components/components/password_generator';
+import { parse } from "tldts";
 
 export default function Site_SignUp() {
-      const userId = "testuserid2"
-      const userIdEncrypted = "HCxyVsfCvxXKyw/rMRpuTJv99PHUrIZhJHICv4zgkMTHXBtgHJi2"
+      const UUID = "f98699a0-d010-4a68-833e-fc9cbbcdf800"
+      const userIdEncrypted = "DSvanR4myji5VmzC06OutTH+nsVUbcOc0pqP7iLPgePsrNj5obsMXkZd+yI9m6MRzf/Xeq921dzq73rvTi/zEQ=="
+      // valid simple pass for testing is Passpass@1
   
       const [keyString, setKeyString] = useState("");
   
@@ -17,10 +19,33 @@ export default function Site_SignUp() {
         const decryptedText = await decrypt(userIdEncrypted, keyString);
         console.log("Decrypted Data: " + decryptedText);
   
-        if(decryptedText === userId){
+        if(decryptedText === UUID){
           console.log("Valid Simple passphrase: User Authenticated")
+
+          const domain = parse(window.location.href).domain;
+          console.log("Parsed Domain:", domain);
+          try {
+            const response = await fetch("https://8fy84busdk.execute-api.us-east-1.amazonaws.com/API/insertDomainName", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                UUID,
+                domain
+              })
+            });
+            if (!response.ok) {
+              throw new Error("Failed to add domain to DB");
+            }
+            const result = await response.json();
+            console.log("Domain successfully added:", result);
+          } catch (err) {
+            console.error("Error adding domain:", err);
+          }
+
           const password = await calculatePassword(keyString);
-          console.log("Password String: ",password)
+          console.log("Password String: ", password)
   
           chrome.runtime.sendMessage({
             action: "fillPassword",
