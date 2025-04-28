e73747730c34577fe1e39e97477c21615ee3e1fd
Xavier Callait <xaviercallait@gmail.com>
2025-04-28 13:57:51 -0400

    Forgot last change

--- aws/src/lambdas/getUserInfo/index.ts
+++ aws/src/lambdas/getUserInfo/index.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { APIGatewayEvent, APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-=======
-import {APIGatewayProxyEventV2,APIGatewayProxyResult} from "aws-lambda";
->>>>>>> main
 import { createConnection } from 'mysql2/promise';
 
 const dbConfig = {
